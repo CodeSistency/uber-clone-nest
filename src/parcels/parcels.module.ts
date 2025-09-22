ef1d2c25afe45279016268540cb361cc06fd2143
CodeSistency <valero.jozee@gmail.com>
2025-09-22 15:20:04 -0400

    Error en Errand

--- src/parcels/parcels.module.ts
+++ src/parcels/parcels.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ParcelsService } from './parcels.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { WebSocketModule } from '../websocket/websocket.module';
 
 @Module({
-  imports: [PrismaModule, NotificationsModule],
+  imports: [PrismaModule, NotificationsModule, WebSocketModule],
   providers: [ParcelsService],
   exports: [ParcelsService],
 })
